98022ada0b2666c63e11a527d7792e8628964fe2
Mar Aguayo Florit <154342626+magufl@users.noreply.github.com>
2024-10-06 10:24:34 +0000

    añadido ternario localstorage

--- src/front/js/pages/private.js
+++ src/front/js/pages/private.js
@@ -6,8 +6,18 @@ export const Private = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="text-center mt-5">
-			<h1>pagina secretíssima</h1>
+		<div>
+			{localStorage.getItem("user") ?
+				<div className="text-center mt-5">
+					<h1>Estás Logueado. Esta es la pagina secretíssima</h1>
+				</div>
+			:
+				<div className="text-center mt-5">
+					<h1>No estás Logueado. No tienes acceso al contenido secreto</h1>
+				</div>
+			}
 		</div>
+		
 	);
+
 };
